8e0cf4c04d704c4edba5b4f2dfcea17e1a962057
mazahir7 <91468649+mazahir7@users.noreply.github.com>
2023-11-09 16:41:48 +0530

    restored (to default) script.js

--- script.js
+++ script.js
@@ -3,27 +3,23 @@
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
 
+
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
-fill.addEventListener("touchstart", touchStart);
-fill.addEventListener("touchend", touchEnd);
-
 for (const empty of empties) {
+
   empty.addEventListener("dragover", dragOver);
   empty.addEventListener("dragenter", dragEnter);
   empty.addEventListener("dragleave", dragLeave);
   empty.addEventListener("drop", dragDrop);
-
-  empty.addEventListener("touchmove", touchMove);
-  empty.addEventListener("touchstart", touchEnter);
-  empty.addEventListener("touchend", touchLeave);
-  empty.addEventListener("touchend", touchDrop);
 }
+
 
 function dragStart() {
   this.className += " hold";
-  setTimeout(() => (this.className = "invisible"), 0);
+
+  setTimeout(() => this.className = "invisible", 0);
 }
 
 function dragEnd() {
@@ -48,33 +44,6 @@ function dragLeave() {
 }
 
 function dragDrop() {
-  this.className = "empty";
-  this.append(fill);
-}
-
-function touchStart() {
-  this.className += " hold";
-  setTimeout(() => (this.className = "invisible"), 0);
-}
-
-function touchEnd() {
-  this.className = "fill";
-}
-
-function touchMove(e) {
-  e.preventDefault();
-  // Handle touch move if needed
-}
-
-function touchEnter() {
-  this.className += " hovered";
-}
-
-function touchLeave() {
-  this.className = "empty";
-}
-
-function touchDrop() {
   this.className = "empty";
   this.append(fill);
 }
